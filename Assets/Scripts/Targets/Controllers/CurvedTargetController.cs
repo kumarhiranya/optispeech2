88d8229a7407bc38e5fcea73904642f58f7aacf7
Optispeech <Optispeech@BBS74487.campus.ad.utdallas.edu>
2020-09-15 15:52:22 -0500

    Changed algo to pause, fixed issues with pause not working as intended

--- Assets/Scripts/Targets/Controllers/CurvedTargetController.cs
+++ Assets/Scripts/Targets/Controllers/CurvedTargetController.cs
@@ -19,13 +19,13 @@ namespace Optispeech.Targets.Controllers {
         /// </summary>
         public Vector3 startPosition;
         public float hAmp, vAmp, frequency;
-        public int pauseTime = 200; // pauseTime in ms
+        public int pauseTime = 0; // pauseTime in ms
 
-        private float angle, angularSpeed, rawAngle, prevRawAngle = 0f; //angle and angleThresh in degrees, angular speed in radians/sec
+        private float angle, angularSpeed; //angle and angleThresh in degrees, angular speed in radians/sec
         private Vector3 ellipseCenter, ellipseRadius, currPosition, prevPosition;
         private bool init = true;
-        private long selfTime, startTime = 0;
-        private int nPauses=0;
+        // private long selfTime, startTime = -1;
+        private int singleOscTime, timeSinceOsc;
         
         public Vector3 GetEllipseRadius(float hAmp, float vAmp)
         {
@@ -51,7 +51,7 @@ namespace Optispeech.Targets.Controllers {
         public float GetAngle(float angularSpeed, long currTime)
         {  
             //Returns angle in degrees
-            float rawAngle = angularSpeed * currTime / 1000;
+            float rawAngle = angularSpeed * 180 * currTime / (1000 * Mathf.PI);
             float angle = rawAngle % 180;
             if (angle < 90) { angle = 180 - angle; }
             return angle;
@@ -72,26 +72,22 @@ namespace Optispeech.Targets.Controllers {
         [HideInDocumentation]
         public override Vector3 GetTargetPosition(long currTime)
         {
-            Debug.Log(string.Format("Parsed values from GetTargetPosition: Startposition:{0}, {1}, {2}, vAmp:{3}, hAmp:{4}, freq:{5}", startPosition.x, startPosition.y, startPosition.z, vAmp, hAmp, frequency));
-            selfTime = currTime - nPauses*pauseTime;
+            pauseTime=250;
+            // Debug.Log(string.Format("Parsed values from GetTargetPosition: Startposition:{0}, {1}, {2}, vAmp:{3}, hAmp:{4}, freq:{5}, pauseTime:{6}", 
+            startPosition.x, startPosition.y, startPosition.z, vAmp, hAmp, frequency, pauseTime));
+            
             angularSpeed = GetAngularSpeed(frequency);
-            rawAngle = angularSpeed * selfTime/1000;
-            if (rawAngle<prevRawAngle){
-                startTime = currTime;             
-            }
-            if(startTime>0 && currTime-startTime<pauseTime) return  prevPosition;
-            else if(startTime>0 && currTime-startTime>=pauseTime){
-                startTime = 0;
-                nPauses++;
-            }
-
+            singleOscTime = (int) (1000/frequency);
+            
             ellipseCenter = GetEllipseCenter(startPosition, hAmp);
             ellipseRadius = GetEllipseRadius(hAmp, vAmp);
-            Debug.Log(string.Format("Calculated values: ellipseCenter:{0}, {1}, {2}, angularSpeed:{3}, ellipseRadius:{4}, {5}, {6}", ellipseCenter.x, ellipseCenter.y, ellipseCenter.z, angularSpeed,
-            ellipseRadius.x, ellipseRadius.y, ellipseRadius.z));       
-            angle = GetAngle(angularSpeed, selfTime);
-            currPosition = PointOnEllipse(ellipseCenter, ellipseRadius, angle);
-            prevRawAngle = rawAngle;
+            // Debug.Log(string.Format("Calculated values: ellipseCenter:{0}, {1}, {2}, angularSpeed:{3}, ellipseRadius:{4}, {5}, {6}", ellipseCenter.x, ellipseCenter.y, ellipseCenter.z, angularSpeed,
+            // ellipseRadius.x, ellipseRadius.y, ellipseRadius.z));       
+            
+            angle = GetAngle(angularSpeed, currTime);
+            timeSinceOsc = (int) (currTime % (singleOscTime/2));
+            if(pauseTime>0 && timeSinceOsc>=0 && timeSinceOsc<pauseTime) currPosition = prevPosition;
+            else currPosition = PointOnEllipse(ellipseCenter, ellipseRadius, angle);
             prevPosition = currPosition;
             if(init) init = false;
             return currPosition;
